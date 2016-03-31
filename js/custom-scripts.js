4f8340e1ad7c784e8ef3cc9ff7d9f2c35f7ede13
bIropka <biropka@gmail.com>
2016-03-31 17:25:08 +0300

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -5,4 +5,27 @@ $(document).ready(function () {
 
     );
 
+    jQuery('.timer').eTimer({
+        etType: 0,
+        etDate: "01.04.2016.0.0",
+        etShowSign: 1,
+        etSep: ":",
+        etTextColor: "#ffffff",
+        etPaddingTB: 15,
+        etPaddingLR: 15,
+        etBackground: "transparent",
+        etBorderColor: "white",
+        etShadow: " 0px 0px 0px 0px #333333",
+        etLastUnit: 3,
+        etNumberSize: 48,
+        etNumberColor: "white",
+        etNumberPaddingTB: 0,
+        etNumberPaddingLR: 2,
+        etNumberBackground: "transparent",
+        etNumberBorderSize: 0,
+        etNumberBorderRadius: 5,
+        etNumberBorderColor: "white"
+    });
+
+
 });
